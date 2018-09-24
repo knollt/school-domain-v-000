9fdb753ae598e8a1751dfd7721b4c5d1397e0b00
Tim Knoll <timothy.knoll@gmail.com>
2018-09-24 17:21:59 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -20,8 +20,13 @@ class School
     @roster[grade] << name
   end
 
-  def grade(number)
-    @roster[number]
+ #  def add_student(student_name, grade)
+ #   roster[grade] ||= []
+ #   roster[grade] << student_name
+ # end
+
+  def grade(student_grade)
+    @roster[student_grade]
   end
 
   def sort 
